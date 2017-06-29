589158d639ec7bcf9fa5dc91ecdb95327f745e35
StenPelzer <pelzersten@gmail.com>
2017-06-29 14:59:45 +0200

    Some small features
    Highscore list, lives display, score in end screen and more

--- BeatieBoysComplete/Assets/Scripts/ScoreScript.cs
+++ BeatieBoysComplete/Assets/Scripts/ScoreScript.cs
@@ -8,15 +8,21 @@ public class ScoreScript : MonoBehaviour {
     public static int scoreValue = 0;
     public static int scoreMultiplier = 1;
     Text score;
+    Text lives;
 
 	// Use this for initialization
-	void Start () {
-        score = GetComponent<Text>();
+	void Start ()
+    {
+        GameObject scoreText = GameObject.Find("ScoreText");
+        GameObject livesText = GameObject.Find("LivesText");
+        score = scoreText.GetComponent<Text>();
+        lives = livesText.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         score.text = "Score: " + scoreValue;
+        lives.text = "Lives: " + GameController.player_health;
 	}
 
     public static void Add()
